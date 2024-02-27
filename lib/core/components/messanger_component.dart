962bebe823acd472b442101a1a13f05674b21a0e
mo_pc <flutterwork28@gmail.com>
2024-02-27 19:45:55 +0200

    messenger component

---
+++ lib/core/components/messanger_component.dart
@@ -0,0 +1,20 @@
+import 'package:e_commerce_app/core/components/custom_text.dart';
+import 'package:e_commerce_app/core/utils/constants.dart';
+import 'package:flutter/material.dart';
+
+class MessengerComponent extends StatelessWidget {
+  final String mess;
+  const MessengerComponent({super.key, required this.mess});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CustomText(
+        text: mess,
+        fontSize: 25,
+        fontFamily: kPacifico,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
