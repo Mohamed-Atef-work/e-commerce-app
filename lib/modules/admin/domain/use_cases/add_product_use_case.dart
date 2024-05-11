da18a9320b602506e37ba390eb639473d39f1703
mo_pc <flutterwork28@gmail.com>
2024-05-11 09:30:24 +0300

    addProductUseCase

--- lib/modules/admin/domain/use_cases/add_product_use_case.dart
+++ lib/modules/admin/domain/use_cases/add_product_use_case.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:dartz/dartz.dart';
 import 'package:e_commerce_app/core/error/failure.dart';
 import 'package:e_commerce_app/core/use_case/base_use_case.dart';
 import 'package:e_commerce_app/modules/admin/data/model/product_model.dart';
 import 'package:e_commerce_app/modules/admin/domain/repository/admin_domain_repository.dart';
 import 'package:e_commerce_app/modules/shared/domain/repository/shared_domain_repo.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class AddProductUseCase extends BaseUseCase<void, AddProductParams> {
   final AdminRepositoryDomain domainRepository;
@@ -24,14 +27,69 @@ class AddProductParams {
   });
 }
 
-class AddProductUseCaseNew extends BaseUseCase<void, AddProductParams> {
-  final AdminRepositoryDomain _domainRepository;
-  final SharedDomainRepo _sharedDomainRepo;
+class AddProductUseCaseNew extends BaseUseCase<void, AddProductParamsNew> {
+  final AdminRepositoryDomain _adminRepo;
+  final SharedDomainRepo _sharedRepo;
 
-  AddProductUseCaseNew(this._domainRepository, this._sharedDomainRepo);
+  AddProductUseCaseNew(this._adminRepo, this._sharedRepo);
 
   @override
-  Future<Either<Failure, void>> call(AddProductParams params) async {
-    return await _domainRepository.addProduct(params);
+  Future<Either<Failure, void>> call(AddProductParamsNew params) async {
+    final pickEither = await _sharedRepo.pickGalleryImage();
+
+    return pickEither.fold(
+      (pickFailure) => Left(pickFailure),
+      (imageFile) async => await _afterPickingImage(params, imageFile),
+    );
   }
+
+  _afterPickingImage(AddProductParamsNew params, File imageFile) async {
+    final uploadEither = await _sharedRepo.uploadImage(imageFile);
+
+    return uploadEither.fold(
+      (uploadImageFailure) => Left(uploadImageFailure),
+      (imageRef) async => await _afterUploadingImage(params, imageRef),
+    );
+  }
+
+  _afterUploadingImage(AddProductParamsNew params, Reference imageRef) async {
+    final downloadUrlEither = await _sharedRepo.downloadImageUrl(imageRef);
+    return downloadUrlEither.fold(
+      (urlFailure) => Left(urlFailure),
+      (imageUrl) async => await _addProduct(params, imageUrl),
+    );
+  }
+
+  _addProduct(AddProductParamsNew params, String imageUrl) async {
+    final addParams = _addParams(params, imageUrl);
+
+    return await _adminRepo.addProduct(AddProductParams(product: addParams));
+  }
+
+  _addParams(AddProductParamsNew params, String imageUrl) => ProductModel(
+        description: params.description,
+        category: params.category,
+        location: params.location,
+        price: params.price,
+        name: params.name,
+        image: imageUrl,
+      );
+}
+
+class AddProductParamsNew {
+  final String description;
+  final String category;
+  final String location;
+  final String name;
+  final File image;
+  final num price;
+
+  AddProductParamsNew({
+    required this.description,
+    required this.location,
+    required this.category,
+    required this.image,
+    required this.price,
+    required this.name,
+  });
 }
